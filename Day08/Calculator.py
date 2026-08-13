16b512b4d4c6d052ac82a5245b6e6b0b0c4d11e0
Rohith Varma <rohithvarma2200@gmail.com>
2026-08-12 20:59:18 -0500

    calculator program alter method

--- Day08/Calculator.py
+++ Day08/Calculator.py
@@ -3,7 +3,7 @@
     and return the value
     make sure you are using constructor, instance methods, class methods, static methods, class variables, local variables
 """
-class Calculator:
+"""class Calculator:
 
     Brand = "Casio"
 
@@ -41,4 +41,46 @@ cal1 = Calculator(num1,num2)
 print("Addition of 2 numbers is: ", cal1.add())
 print("Multiplication of 2 numbers is: ", cal1.Multiply())
 print("Subtraction of 2 numbers is: ", cal1.subtract())
-print("Divide of 2 numbers is: ", cal1.divide())
+print("Divide of 2 numbers is: ", cal1.divide())"""
+
+class calculator:
+
+    Brand = "Casio"
+
+    def __init__(self):
+        self.add = 0
+        self.mul = 0
+        self.sub = 0
+        self.div = 0
+
+    def action(self,num1, num2):
+        self.add = num1 + num2
+        self.mul = num1 * num2
+        self.sub = num1 - num2
+        if num2 != 0:
+            self.div = num1 / num2 
+        else:
+            self.div = "Cannot divide by zero"
+
+        print(f"\nResults:")
+        print(f"Addition: {self.add}")
+        print(f"Multiplication: {self.mul}")
+        print(f"Subtraction: {self.sub}")
+        print(f"Division: {self.div}")
+
+    @classmethod
+    def Clsaction(cls):
+        print(f"\nWelcome to {cls.Brand} calculator")
+
+calculator.Clsaction()
+
+num1 = int(input("\nenter num1 : "))
+num2 = int(input("\nenter num2 : "))
+
+# 3. Create an instance of the calculator
+my_calc = calculator()
+
+# 4. Perform the action
+my_calc.action(num1, num2)
+
+        
